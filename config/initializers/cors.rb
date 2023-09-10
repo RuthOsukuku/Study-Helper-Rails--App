b8f5f690fbaafc6aaacba62ca4895788442398b8
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-10 15:20:55 +0300

    modified cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # Replace with your actual frontend origin
+    origins "*" # Replace with your actual frontend origin
 
     resource "*",
       headers: :any,
