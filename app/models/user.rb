db40962b6eb65c1bc6b8e73dbab0fd5856546061
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-10 14:53:00 +0300

    modified user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+    #validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
     validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
     validates :password, presence: true, length: { minimum: 6 }
   
